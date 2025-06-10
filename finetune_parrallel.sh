7bf535818923a73a7265205c8e84ff3c020b71a8
norahallqvistMK <nora_hallqvist@mckinsey.com>
2025-06-10 09:42:01 +0100

    removed parralel files

--- finetune_parrallel.sh
+++
@@ -1,9 +0,0 @@
-python finetune_gpu.py \
-    --pretrain_ckpt longformer_ckpt/recformer_seqrec_ckpt.bin \
-    --data_path finetune_data_trans/ \
-    --num_train_epochs 20 \
-    --batch_size 10 \
-    --device 1 \
-    --fp16 \
-    --finetune_negative_sample_size -1 \
-    --use_multi_gpu
